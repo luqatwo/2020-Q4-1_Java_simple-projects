172ef1bf02f274ca084bb7c8f775b2ff7e6d8b34
luqatwo <155319133+luqatwo@users.noreply.github.com>
2024-01-15 01:14:48 +0100

    Add files via upload

---
+++ 1_flight-search-engine/src/Flight.java
@@ -0,0 +1,24 @@
+class Flight {
+  String departue;
+  String arrival;
+  public Flight (String departue, String arrival) {
+    this.departue = departue;
+    this.arrival = arrival;
+  }
+  
+  /// public void displayValues() {
+    /// System.out.println ("Flight from " + this.departue + " to " + this.arrival);
+  /// }
+  
+  /// < komentarz 4-7... ponowione wyświetlanie tekstu - odniesienie do kodu z głównej klasy Main >
+  
+  /// public String getParameters() {
+    /// return "Flight from " + this.departue + " to " + this.arrival;
+  /// }
+  
+  /// < komentarz 5-7... ponowione wyświetlanie tekstu - odniesienie do kodu z głównej klasy Main >
+  
+  /// < komentarz 6-7... do uzupełnienia >
+  /// < komentarz 7-7... do uzupełnienia >
+
+}
--- 1_flight-search-engine/src/Main.java
+++ 1_flight-search-engine/src/Main.java
@@ -1,28 +1,27 @@
 class Main {
   public static void main(String[] args) {
     Flight plane = new Flight ("Gdansk", "Warsaw");
+    System.out.println ("");
     System.out.println ("Flight from " + plane.departue + " to " + plane.arrival);
-    plane.displayValues();
-    String parameters = plane.getParameters();
-    System.out.println (parameters);
-    
-  }
-}
+    System.out.println ("");
 
-class Flight {
-  String departue;
-  String arrival;
-  
-  public Flight (String departue, String arrival) {
-    this.departue = departue;
-    this.arrival = arrival;
-  }
-  
-  public void displayValues() {
-    System.out.println ("Flight from " + this.departue + " to " + this.arrival);
-  }
-  
-  public String getParameters() {
-    return "Flight from " + this.departue + " to " + this.arrival;
+    /// < komentarz 1-7 [A]... wyświetlenie tekstu o locie z miasta A do miasta B >
+
+    /// plane.displayValues();
+    
+    /// String parameters = plane.getParameters();
+    /// System.out.println (parameters);
+
+    /// < komentarz 2-7... ponowione wyświetlanie tekstu - odniesienie do kodu z klasy Flight >
+
+    String first = new String ("test");
+    String second = new String ("test");
+    if (first.equals(second)) {
+      System.out.println ("Cities are the same.");
+    } else {
+      System.out.println ("Cities are different.");
+    }
+
+    /// < komentarz 3-7... powyższy kod do poprawy >
   }
 }
